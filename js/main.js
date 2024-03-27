bd639669d41cae363603b17f459a4114a8aff78f
mimi0908 <emilygarciamontes@gmail.com>
2024-03-27 17:27:29 -0500

    agregando anexos de pagina

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-let posActual=0;
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
+import { glosary } from "./baseDatos.js";
+let active = 0;
+
+function cargarTarjeta(active) {
+  document.getElementById("title").innerHTML = glosary[active].word;
+  document.getElementById("fonetica").innerHTML = glosary[active].phonetic;
+  document.getElementById("definition").innerHTML = glosary[active].explain;
+  document.getElementById("contador").innerHTML = glosary[active].num;
+}
+
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
 next.onclick = function () {
-    active = active + 1 < items.length ? active + 1 : active;
-}
+    document.getElementById("fonetica").className="fonetica";
+    active = (active + 1) % glosary.length;
+    cargarTarjeta(active);
+};
 prev.onclick = function () {
-    active = active - 1 >= 0 ? active - 1 : active;
+    document.getElementById("fonetica").className="fonetica";
+    active = (active - 1 + glosary.length) % glosary.length;
+    cargarTarjeta(active);
+};
+
+cargarTarjeta(active);
+
+let audio= document.getElementById("audio");
+
+audio.onclick= function(){
+    document.getElementById("fonetica").className="fonetica-active";
 }
